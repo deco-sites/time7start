065260027d7cdd6907dc7878bebece59fa26d0f0
rfaaelnc <rfaaelnc@gmail.com>
2023-05-21 10:58:06 -0300

    adjusting footer

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer({ text = "© 2023" }: Props) {
   return (
     <footer className="w-full mt-auto">
       <div className="container mx-auto px-4 flex justify-center h-12">
-        <p>{text}</p>
+        <p className="text-[#c0c0c0]">{text}</p>
       </div>
     </footer>
   );
